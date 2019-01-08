b0506bdf3acea4cc569edc9d70f62cf7701ebaf7
ilovelili <route666@live.cn>
2019-01-08 19:25:01 +0900

    update database schema

--- database/dongfeng_core_schema.sql
+++ database/dongfeng_core_schema.sql
@@ -91,6 +91,14 @@ CREATE TABLE `namelists` (
   UNIQUE KEY `year_class_name_UNIQUE` (`year`,`class_id`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `ingredients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `recipes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
